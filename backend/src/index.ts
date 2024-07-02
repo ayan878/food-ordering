812b0f57f8046eea694180ae00e6b83a7c8a7886
ayan878 <ayanraza197@gmail.com>
2024-07-03 02:14:26 +0530

    backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,17 @@
-import express, { Request, Response } from "express"; // Importing express and types for Request and Response
-import cors from "cors"; // Importing cors middleware
-import "dotenv/config"; // Loading environment variables from a .env file
+import express, { Request, Response } from "express"; 
+import cors from "cors"; 
+import "dotenv/config";
 
-const app = express(); // Creating an instance of express
+const app = express(); 
+app.use(express.json());
+app.use(cors()); 
 
-app.use(express.json()); // Middleware to parse JSON request bodies
-app.use(cors()); // Middleware to enable CORS (Cross-Origin Resource Sharing)
-
-// Route to handle GET requests to "/test"
 app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello" }); // Responding with a JSON object
+  res.json({ message: "Hello" }); 
 });
 
-const port = 7000; // Defining the port number
-
+const port = 7000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`); // Logging server start message
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
