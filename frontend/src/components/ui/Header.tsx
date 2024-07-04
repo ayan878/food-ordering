a8f795a7384c2ec90cd397050846cdb193defaec
ayan878 <ayanraza197@gmail.com>
2024-07-04 23:09:31 +0530

    header

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import MobileNavbar from "./MobileNav";
+import MainNav from "./MainNav";
 
 export default function Header() {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
-      <div className="container mx-auto flex justify-between items-center"><Link to="/" className="text-3xl font-bold  tracking-tight text-orange-500">tastyTrials</Link><div className="md:hidden"><MobileNavbar/></div></div>
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          to="/"
+          className="text-3xl font-bold  tracking-tight text-orange-500"
+        >
+          tastyTrials
+        </Link>
+        <div className="md:hidden">
+          <MobileNavbar />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/components/ui/MainNav.tsx
+++ frontend/src/components/ui/MainNav.tsx
@@ -2,7 +2,7 @@ import { Button } from "./button";
 
 const MainNav = () => {
     return (
-        <Button variant="ghost">
+        <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white">
             Log in
         </Button>
     );
