3b8152345534e0384403e3a05ab96c1b512bd54e
ayan878 <ayanraza197@gmail.com>
2024-07-05 15:38:34 +0530

    frontend

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,11 @@
+import logo from "@/assets/logo.png";
+
 const Footer = () => {
   return (
-    <div className="bg-orange-500 py-10">
+    <div className="bg-orange-500 py-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <span className="text-3xl font-bold text-white tracking-tight">
+        <span className="text-3xl font-bold text-white tracking-tight flex items-center">
+          <img src={logo} alt="" className="w-8 md:w-14" />
           tastyTrials.com
         </span>
         <span className="text-white tracking-tight flex gap-4 font-bold">
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
 import MobileNavbar from "./MobileNav";
 import MainNav from "./MainNav";
+import logo from "@/assets/logo.png"
 
 export default function Header() {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6">
+    <div className="border-b-2 border-b-orange-500 py-2">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-bold  tracking-tight text-orange-500"
-        >
+          className="text-3xl font-bold  tracking-tight text-orange-500 flex items-center"
+        ><img src={logo} alt="" className="w-8 md:w-14"/>
           tastyTrials
         </Link>
         <div className="md:hidden">
