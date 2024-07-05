2aa9abfb28195fc841508666ca791cc0b14411b7
ayan878 <ayanraza197@gmail.com>
2024-07-05 15:41:10 +0530

    frontend

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="bg-orange-500 py-4">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <span className="text-3xl font-bold text-white tracking-tight flex items-center">
+        <span className="text-2xl md:text-3xl font-bold text-white tracking-tight flex items-center">
           <img src={logo} alt="" className="w-8 md:w-14" />
           tastyTrials.com
         </span>
