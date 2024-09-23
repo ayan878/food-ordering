d5476be93cdd07a1e2ca2c03fc7e35e3b7f30753
ayan878 <ayanraza197@gmail.com>
2024-09-23 17:21:17 +0530

    header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -5,13 +5,13 @@ import MainNav from "./MainNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 border-b-orange-500 py-6 ">
-      <div className="container mx-auto flex justify-between items-center">
+    <div className="border-b-2 border-b-orange-500 py-4 ">
+      <div className="container px-4 md:px-0 mx-auto  flex justify-between items-center">
         <Link
           to="/"
-          className="text-3xl font-bold tracking-tight text-orange-500 flex"
+          className="text-3xl font-bold tracking-tight text-orange-500 flex items-center"
         >
-          <img src={Logo} className="w-8 h-10" />
+          <img src={Logo} className="w-8" />
           tastyTrials
         </Link>
         <div className="md:hidden">
@@ -20,8 +20,6 @@ const Header = () => {
         <div className="hidden md:block">
           <MainNav />
         </div>
-        {/* 
-        <Button className="bg-orange-500 font-bold">Log In</Button> */}
       </div>
     </div>
   );
