5e395adf6711f130600574f42049c220e527c732
ayan878 <ayanraza197@gmail.com>
2024-09-23 15:38:41 +0530

    assets

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,11 @@
+import { Sheet } from "./ui/sheet";
+
+
+const MobileNav = () => {
+    return (
+        <Sheet>
+            <h1>Welcome to tastyTrials</h1>
+        </Sheet>
+    );
+};
+export default MobileNav;
