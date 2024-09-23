00b6a3b038f0b76e9985ed58a8b1c4d178421651
ayan878 <ayanraza197@gmail.com>
2024-09-23 15:55:40 +0530

    Mobile Nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo.png";
+import MobileNav from "./MobileNav";
 const Header = () => {
-    return (
-      <div className="border-b-2 border-b-orange-500 py-6 ">
-        <div className="container mx-auto flex justify-between items-center">
-          <Link to="/"
-            className="text-3xl font-bold tracking-tight text-orange-500 flex"
-          ><img src={Logo} className="w-8 h-10"/>
-            tastyTrials
-          </Link>
-          <Button className="bg-orange-500 font-bold">Log In</Button>
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6 ">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          to="/"
+          className="text-3xl font-bold tracking-tight text-orange-500 flex"
+        >
+          <img src={Logo} className="w-8 h-10" />
+          tastyTrials
+        </Link>
+        <div className="md:hidden">
+          <MobileNav />
         </div>
+        {/* 
+        <Button className="bg-orange-500 font-bold">Log In</Button> */}
       </div>
-    );
+    </div>
+  );
 };
 export default Header;
--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,11 +1,25 @@
-import { Sheet } from "./ui/sheet";
-
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 
 const MobileNav = () => {
-    return (
-        <Sheet>
-            <h1>Welcome to tastyTrials</h1>
-        </Sheet>
-    );
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcome to tastyTrials</span>
+        </SheetTitle>
+        <Separator/>
+        <SheetDescription className="flex">
+            <Button className="flex-1 bg-orange-500 font-bold">Log In</Button>
+        </SheetDescription>
+        
+      </SheetContent>
+    </Sheet>
+  );
 };
 export default MobileNav;
