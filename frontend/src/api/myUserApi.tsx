4199c0e34ae49196516b0e58975c585f455fdfed
ayan878 <ayanraza197@gmail.com>
2024-10-15 14:36:01 +0530

    api

---
+++ frontend/src/api/myUserApi.tsx
@@ -0,0 +1,40 @@
+import { useMutation } from "react-query";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log(API_BASE_URL);
+
+
+type CreateUserRequest = {
+  auth0Id: string;
+  email: string;
+};
+
+export const useCreateMyUser = () => {
+  const createMyUserRequest = async (user: CreateUserRequest) => {
+    const response = await fetch("http://localhost:3000/api/my/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to create user");
+    }
+  };
+
+  const {
+    mutateAsync: createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useMutation(createMyUserRequest);
+
+  return {
+    createUser,
+    isLoading,
+    isError,
+    isSuccess,
+  };
+};
