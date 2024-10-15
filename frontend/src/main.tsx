80065d376c31deedf186d70ad559a4967a4f8e8d
ayan878 <ayanraza197@gmail.com>
2024-10-15 14:36:07 +0530

    main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./global.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes.tsx";
-import Auth0ProviderWithNavigate from "./auth/auth0ProviderWithNavigate.tsx";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate.tsx";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
