390df81a7040984a14bbe493715ed2f8fb08d525
ayan878 <ayanraza197@gmail.com>
2024-10-16 22:17:23 +0530

    user-controller

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
-import userModel from "../models/user"; 
+import userModel from "../models/user";
 
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.body;
     const existingUser = await userModel.findOne({ auth0Id });
-    
+
     if (existingUser) {
       return res.status(200).send();
     }
-    
+
     const newUser = new userModel(req.body);
     await newUser.save();
-    
+
     res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
