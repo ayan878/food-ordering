64f78e33c72b07d763397d4f2d37d1bdd32f17e4
ayan878 <ayanraza197@gmail.com>
2024-10-17 19:20:37 +0530

    routes

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,9 +1,11 @@
 import Express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 
 const router = Express.Router();
 
 router.post("/",jwtCheck, MyUserController.createCurrentUser);
+router.post("/",jwtCheck,jwtParse, MyUserController.updateCurrentUser);
+
 
 export default router;
